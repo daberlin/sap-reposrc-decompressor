03afbf7dba2a9835147f537e8bec69a090e60a4a
dberlin <>
2012-06-24 10:30:51 +0000

    - initial commit of dummy program and required libraries

---
+++ SAP-Report-Source-Decompressor/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -x
+
+g++ -o sap-reposrc-decompressor -O2 *.cpp lib/*.cpp
---
+++ SAP-Report-Source-Decompressor/sap-reposrc-decompressor.cpp
@@ -0,0 +1,15 @@
+// Thrown together by Daniel Berlin <mail@daniel-berlin.de> - http://www.daniel-berlin.de/
+// Inspired by code from Dennis Yurichev <dennis@conus.info> - http://www.yurichev.com/
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lib/hpa101saptype.h"
+#include "lib/hpa106cslzc.h"
+#include "lib/hpa107cslzh.h"
+#include "lib/hpa104CsObject.h"
+#include "lib/hpa105CsObjInt.h"
+
+int main(int argc, char *argv[]) {
+}
