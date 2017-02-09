3b85b9ebfbfe332683d05e96d8fc920436aa0049
Daniel Berlin <mail@daniel-berlin.de>
2017-02-09 19:55:09 +0100

    Tune output buffer factor; various minor adjustments

--- SAP-Report-Source-Decompressor/sap-reposrc-decompressor.cpp
+++ SAP-Report-Source-Decompressor/sap-reposrc-decompressor.cpp
@@ -1,10 +1,10 @@
 //--------------------------------------------------------------------------------------------------
 // SAP Source Code Decompressor
 //--------------------------------------------------------------------------------------------------
-// Written by Daniel Berlin <mail@daniel-berlin.de> - http://www.daniel-berlin.de/
+// Written by Daniel Berlin <mail@daniel-berlin.de> - https://www.daniel-berlin.de/
 // Inspired by code from Dennis Yurichev <dennis@conus.info> - http://www.yurichev.com/
-// This program uses portions of the MaxDB 7.6.00.34 source code, which are licensed under
-// the GNU General Public License (see file headers in "lib/" folder).
+// This program uses portions of the MaxDB 7.6 source code, which are licensed under
+// the GNU General Public License v2 (see file headers in "lib/" folder).
 // Comments, suggestions and questions are welcome!
 //--------------------------------------------------------------------------------------------------
 // The source code stored in REPOSRC-DATA is basically compressed using the LZH algorithm
@@ -18,10 +18,11 @@
 //   2012-06-23 - 1.0.0 - Initial version
 //   2012-06-24 - 1.0.1 - Revert modifications to MaxDB library
 //   2015-01-05 - 1.1.0 - Major UTF-16 enhancement by Uwe Lindemann
-//   2015-07-24 - 1.1.1 - Add option for non-unicode SAP systems by Bastian Preißler
+//   2015-07-24 - 1.1.1 - Add option for non-Unicode SAP systems by Bastian Preißler
+//   2017-02-09 - 1.1.2 - Tune output buffer factor; various minor adjustments
 //--------------------------------------------------------------------------------------------------
 
-#define VERSION "1.1.1"
+#define VERSION "1.1.2"
 
 // Silence MS Visual C++ ("... consider using fopen_s instead ...")
 #ifdef _MSC_VER
@@ -44,7 +45,7 @@ int main(int argc, char *argv[]) {
 	FILE *fin, *fout;				// In-/output file handles
 	long fin_size;					// Input file size
 	SAP_BYTE *bin, *bout;			// In-/output buffers
-	short factor = 10;				// Initial output buffer size factor
+	short factor = 5;				// Initial output buffer size factor
 	class CsObjectInt o;			// Class containing the decompressor
 	SAP_INT byte_read, byte_decomp;	// Number of bytes read and decompressed
 	long i;							// Loop counter
@@ -53,8 +54,8 @@ int main(int argc, char *argv[]) {
 	char cuc[] = "-u";				// Unicode parameter
 	int funicode;					// Unicode flag
 
-	char nuc[] = "-n";				// Non-Unicode SAP system parameter
-	int fnuc;						// Non-Unicode SAP system flag
+	char nuc[] = "-n";				// non-Unicode SAP system parameter
+	int fnuc;						// non-Unicode SAP system flag
 
 	unsigned char cbom1 = (unsigned char) 0xFE;		// BOM for UTF-16: 0xFEFF
 	unsigned char cbom2 = (unsigned char) 0xFF;		// ...
@@ -64,22 +65,22 @@ int main(int argc, char *argv[]) {
 	printf("------------------------------------\n\n");
 
 	// Check command line parameters
-	if (argc < 3 || argc > 4 || argv[1] == NULL || argv[2] == NULL) {
+	if(argc < 3 || argc > 4 || argv[1] == NULL || argv[2] == NULL) {
 		printf("Usage:\n  %s <infile> <outfile> [-u] [-n]\n\n", argv[0]);
 		printf("Options:\n  -u : create UTF-16 output; defaults to ASCII\n");
-		printf("  -n : assume input from non-unicode SAP system\n\n");
+		printf("  -n : assume input from non-Unicode SAP system\n\n");
 		return 0;
 	}
 
-	if (argc == 4 && strcmp(argv[3], cuc) == 0) { funicode = 1; }
-	else                                        { funicode = 0; }
+	if(argc == 4 && strcmp(argv[3], cuc) == 0) { funicode = 1; }
+	else                                       { funicode = 0; }
 
-	if (argc == 4 && strcmp(argv[3], nuc) == 0) { fnuc = 1; }
-	else										{ fnuc = 0; }
+	if(argc == 4 && strcmp(argv[3], nuc) == 0) { fnuc = 1; }
+	else                                       { fnuc = 0; }
 
 	// Open input file
 	fin = fopen(argv[1], "rb");
-	if (fin == NULL) {
+	if(fin == NULL) {
 		printf("Error opening input file '%s'\n", argv[1]);
 		return 1;
 	}
@@ -103,7 +104,7 @@ int main(int argc, char *argv[]) {
 
 	// Create output file
 	fout = fopen(argv[2], "wb");
-	if (fout == NULL) {
+	if(fout == NULL) {
 		printf("Error creating output file '%s'\n", argv[2]);
 		free(bin);
 		return 2;
@@ -115,13 +116,13 @@ int main(int argc, char *argv[]) {
 	ret = o.CsGetAlgorithm(bin + 1);
 	printf("Algorithm: %i (1 = LZC, 2 = LZH)\n", ret);
 
-	for (;;) {
-		// Create output buffer with an initial size factor of 10 x input buffer
+	for(;;) {
+		// Create output buffer with an initial size of <factor> x <input buffer size>
 		bout = (SAP_BYTE*) malloc(fin_size * factor);
 
 		// Perform decompression
 		ret = o.CsDecompr(
-			bin + 1					// Skip 1st byte (-> strange !)
+			bin + 1					// Skip 1st byte (-> strange)
 			, fin_size - 1			// Input size
 			, bout					// Output buffer
 			, fin_size * factor		// Output buffer size
@@ -132,15 +133,16 @@ int main(int argc, char *argv[]) {
 
 		// Output buffer too small -> increase size factor and retry
 		if(ret == CS_END_OUTBUFFER || ret == CS_E_OUT_BUFFER_LEN) {
+		//	printf("Output buffer adjusted: factor %i -> %i\n", factor, factor + 5);
+			factor += 5;
 			free(bout);
-			factor += 10;
 			continue;
 		}
 
 		free(bin);
 
 		// Handle all other return codes
-		switch (ret) {
+		switch(ret) {
 			case CS_END_OF_STREAM      :
 			case CS_END_INBUFFER       : printf("Decompression successful\n"     ); break;
 			case CS_E_IN_BUFFER_LEN    : printf("Error: CS_E_IN_BUFFER_LEN\n"    ); return  3;
@@ -157,41 +159,42 @@ int main(int argc, char *argv[]) {
 	}
 
 	// In case of Unicode output: write UTF-16 BOM
-	if (funicode) {
+	if(funicode) {
 		fwrite(&cbom1, 1, 1, fout);
 		fwrite(&cbom2, 1, 1, fout);
 	}
 
 	// The 2nd byte contains the length of the first line.
-	// For non-unicode SAP systems the 1st byte contains the length of the first line.
+	// For non-Unicode SAP systems the 1st byte contains the length of the first line.
 	// Compute position of next length field.
-	if (fnuc) {
-		nextpos = (long)bout[0];
+	if(fnuc) {
+		nextpos = (long) bout[0];
 	}
-	else	  {
-		nextpos = ((long)bout[1]) * 2 + 3;
+	else {
+		nextpos = ((long) bout[1]) * 2 + 3;
 	}
 
-	for (i = 1; i < byte_decomp; i++) {
-		if (i == 1 && !fnuc) { continue; }
-		if ((i % 2) == 0 && !fnuc) {
-			if (funicode) {
+	for(i = 1; i < byte_decomp; i++) {
+		if(i == 1 && !fnuc) { continue; }
+
+		if((i % 2) == 0 && !fnuc) {
+			if(funicode) {
 				// In case of Unicode output: write Big Endian byte
 				ret = fwrite(bout + i, 1, 1, fout);
 			}
 		}
 		else {
-			if (i == nextpos) {
+			if(i == nextpos) {
 				// Write line feed
 				ret = fwrite("\n", 1, 1, fout);
 
 				// Compute position of next length field
-				if (fnuc) {
-					nextpos = nextpos + (long)bout[0] + 1;
+				if(fnuc) {
+					nextpos = nextpos + (long) bout[0] + 1;
 					i += 1;
 				}
 				else {
-					nextpos = nextpos + (((long)bout[i]) * 2 + 2);
+					nextpos = nextpos + (((long) bout[i]) * 2 + 2);
 				}
 			}
 			else {
@@ -199,7 +202,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		if (ret != 1) {
+		if(ret != 1) {
 			printf("Error writing to output file '%s'\n", argv[2]);
 			return 11;
 		}
@@ -209,8 +212,8 @@ int main(int argc, char *argv[]) {
 	fwrite("\n", 1, 1, fout);
 	fclose(fout);
 
-	if (funicode) { printf("Unicode"   ); }
-	else          { printf("Plain text"); }
+	if(funicode) { printf("Unicode"   ); }
+	else         { printf("Plain text"); }
 	printf(" source written to '%s'\nHave a nice day\n\n", argv[2]);
 
 	free(bout);
